27604dd050d4b28f42df08296c51bbd3b6227e1d
Nikita Solov'ev <nikidry70@gmail.com>
2016-10-18 21:34:39 +0300

    Исправил замечания по тестам
    Если сейчас будет все верно, то мне показывать все равно лабораторную на следующей паре?

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -4,30 +4,31 @@
 
 TEST(TMatrix, can_create_matrix_with_positive_length)
 {
-  ASSERT_NO_THROW(TMatrix<int> m(2));
+	ASSERT_NO_THROW(TMatrix<int> m(2));
 }
 
 TEST(TMatrix, cant_create_too_large_matrix)
 {
-  ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
+	ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
 }
 
 TEST(TMatrix, throws_when_create_matrix_with_negative_length)
 {
-  ASSERT_ANY_THROW(TMatrix<int> m(-5));
+	ASSERT_ANY_THROW(TMatrix<int> m(-5));
 }
 
 TEST(TMatrix, can_create_copied_matrix)
 {
-  TMatrix<int> m(5);
+	TMatrix<int> m(5);
 
-  ASSERT_NO_THROW(TMatrix<int> m1(m));
+	ASSERT_NO_THROW(TMatrix<int> m1(m));
 }
 
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
 	TMatrix<int> m(4);
-	TMatrix<int> m1 = m;
+	m[1][1] = 2;
+	TMatrix<int> m1(m);
 
 	EXPECT_EQ(m, m1);
 }
@@ -69,23 +70,20 @@ TEST(TMatrix, can_set_and_get_element)
 TEST(TMatrix, throws_when_set_element_with_negative_index)
 {
 	TMatrix<int> m;
-	ASSERT_ANY_THROW( m[-2]);
+	ASSERT_ANY_THROW(m[-2]);
 }
 
 TEST(TMatrix, throws_when_set_element_with_too_large_index)
 {
 	TMatrix<int> m;
-	ASSERT_ANY_THROW(m[MAX_MATRIX_SIZE+1]);
+	ASSERT_ANY_THROW(m[MAX_MATRIX_SIZE + 1]);
 }
 
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
 	TMatrix<int> m(4);
-	TMatrix<int> m1(3);
 
-	m = m1;
-
-	EXPECT_EQ(m1, m);
+	ASSERT_NO_THROW(m = m);
 }
 
 TEST(TMatrix, can_assign_matrices_of_equal_size)
@@ -95,8 +93,10 @@ TEST(TMatrix, can_assign_matrices_of_equal_size)
 	TMatrix<int> m(size);
 	TMatrix<int> m1(size);
 
+	m = m1;
+
 	EXPECT_EQ(m, m1);
-  }
+}
 
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
@@ -116,7 +116,9 @@ TEST(TMatrix, can_assign_matrices_of_different_size)
 	TMatrix<int> m(size);
 	TMatrix<int> m1(size + 1);
 
-	EXPECT_NE(m, m1);
+	m = m1;
+
+	EXPECT_EQ(m, m1);
 }
 
 TEST(TMatrix, compare_equal_matrices_return_true)
@@ -126,7 +128,7 @@ TEST(TMatrix, compare_equal_matrices_return_true)
 	TMatrix<int> m(size);
 	TMatrix<int> m1(size);
 
-	EXPECT_EQ(m, m1);
+	EXPECT_EQ(true, m == m1);
 }
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
@@ -135,7 +137,7 @@ TEST(TMatrix, compare_matrix_with_itself_return_true)
 
 	TMatrix<int> m(size);
 
-	EXPECT_EQ(m, m);
+	EXPECT_EQ(true,m==m);
 }
 
 TEST(TMatrix, matrices_with_different_size_are_not_equal)
@@ -172,7 +174,7 @@ TEST(TMatrix, can_add_matrices_with_equal_size)
 	res[1][1] = 2;
 
 	EXPECT_EQ(res, tmp);
-	
+
 }
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
@@ -220,4 +222,3 @@ TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
 
 	ASSERT_ANY_THROW(m1 - m2);
 }
-
